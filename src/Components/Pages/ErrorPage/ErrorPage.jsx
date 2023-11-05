f5f11e83a9bdcf1ec4ebb994930b643ea834bf1c
Sabbir6644 <sabbirmats99@gmail.com>
2023-11-05 19:50:21 +0600

    update  error page and added home button

--- src/Components/Pages/ErrorPage/ErrorPage.jsx
+++ src/Components/Pages/ErrorPage/ErrorPage.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet-async";
+import error from "/src/assets/images/error.png"
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
@@ -7,8 +9,15 @@ const ErrorPage = () => {
                <Helmet>
                     <title>Delicious food | Error</title>
                </Helmet>
-             <h2>Error</h2>  
-          </div>
+                    <div className=" max-w-7xl mx-auto relative w-full rounded-md">
+                         <img src={error} className="w-full" />
+                         <div className=" text-white absolute transform -translate-y-2/3  right-[50%] top-[10%] ">
+                              <Link to={'/'}><button className="py-2 px-4 rounded-md bg-red-700 text-white">Go Home</button></Link>
+                         </div>
+                    </div>
+
+               </div>
+
      );
 };
 
